0ae1b96a3e8d38882264f32a1a1845eb6dd36e95
Tyler Moore <tylermoore@Tylers-iMac.frontierlocal.net>
2020-02-06 01:24:11 -0500

    Add placeholder iframe on each page

--- src/pages/OmoCafe.vue
+++ src/pages/OmoCafe.vue
@@ -1,3 +1,5 @@
 <template>
-  <div class="text-h4">Hello from Omo Cafe Page</div>
+  <div>
+    <iframe src="https://petpartners-readme.netlify.com" height="1000px" width="100%"></iframe>
+  </div>
 </template>
--- src/pages/PetPartners.vue
+++ src/pages/PetPartners.vue
@@ -1,3 +1,5 @@
 <template>
-  <div class="text-h4">Hello from Pet Partners Page</div>
+  <div>
+    <iframe src="https://petpartners-readme.netlify.com" height="1000px" width="100%"></iframe>
+  </div>
 </template>
