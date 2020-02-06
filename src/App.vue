0c83fcd7b2738e429870401c7c0b94741f1edeff
Tyler Moore <tylermoore@Tylers-iMac.frontierlocal.net>
2020-02-06 16:16:47 -0500

    Set dark to true in app.vue

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,18 @@
 
 <script>
 export default {
-  name: 'App',
-
-  meta () {
+  name: "App",
+  created() {
+    this.$q.dark.set(true);
+  },
+  meta() {
     return {
       // fallback metas when not defined in child components.
       // notice also that we removed the 'title' and 'meta description' tags
       // from 'src/index.template.html' to avoid having duplicates
-      title: 'Quasarmarkdown',
-      description: { name: 'description', content: 'A Quasar Framework app' }
-    }
+      title: "Quasarmarkdown",
+      description: { name: "description", content: "A Quasar Framework app" }
+    };
   }
-}
+};
 </script>
